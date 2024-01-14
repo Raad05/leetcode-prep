fcd13417809117bd10dc93ce186418af9a0d9c4e
Raad05 <ashabulraad@gmail.com>
2024-01-14 19:11:48 +0600

    selection sort.py

---
+++ sorting/selection_sort.py
@@ -0,0 +1,16 @@
+array = [5, 9, 4, 8, 6, 1, 12, 10]
+
+def selection_sort(arr):
+    for i in range(len(arr)):
+        min = i
+        for j in range(i, len(arr)):
+            if arr[j] < arr[min]:
+                min = j
+
+        temp = arr[i]
+        arr[i] = arr[min]
+        arr[min] = temp
+
+    return arr
+
+print(selection_sort(array))
