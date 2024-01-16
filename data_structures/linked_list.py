86d2a30103ba90a3f4820a172bedc4861ed20716
Raad05 <ashabulraad@gmail.com>
2024-01-16 23:23:57 +0600

    linked list

---
+++ data_structures/linked_list.py
@@ -0,0 +1,21 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+node1 = Node(10)
+node2 = Node(20)
+node3 = Node(30)
+node4 = Node(40)
+node5 = Node(50)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+
+current  = node1
+
+while current != None:
+    print(current.data, end='->')
+    current = current.next
