679b4559528551375f279f8b287116361bc9f4ed
Raad05 <ashabulraad@gmail.com>
2024-03-28 23:49:31 +0600

    leetcode binary_search_34 complete

--- algorithms/binary_search_34.py
+++ algorithms/binary_search_34.py
@@ -1,8 +1,6 @@
 class Solution(object):
-    def searchRange(self, nums, target):
-        pass
-
-    def binary_search(lo, hi, condition):
+    # generic binary search
+    def binary_search(self, lo, hi, condition):
         while lo <= hi:
             mid = (lo + hi) // 2
             result = condition(mid)
@@ -15,48 +13,41 @@ class Solution(object):
                 lo = mid + 1
 
         return -1
-# def binary_search(lo, hi, condition):
-#     while lo <= hi:
-#         mid = (lo + hi) // 2
-#         result = condition(mid)
+    
+    # find first position
+    def first_position(self, nums, target):
+        # condition to find first position
+        def condition(mid):
+            mid_number = nums[mid]
 
-#         if result == 'found':
-#             return mid
-#         elif result == 'left':
-#             hi = mid - 1
-#         elif result == 'right':
-#             lo = mid + 1
+            if mid_number == target:
+                if mid > 0 and nums[mid - 1] == target:
+                    return 'left'
+                else:
+                    return 'found'
+            elif mid_number > target:
+                return 'left'
+            elif mid_number < target:
+                return 'right'
+        return self.binary_search(0, len(nums) - 1, condition)
+    
+    # find last position
+    def last_position(self, nums, target):
+        # condition to find first position
+        def condition(mid):
+            mid_number = nums[mid]
 
-#     return -1
-
-# def first_position(nums, target):
-#     def condition(mid):
-#         if nums[mid] == target:
-#             if mid > 0 and nums[mid-1] == target:
-#                 return 'left'
-#             return 'found'
-#         elif nums[mid] < target:
-#             return 'right'
-#         else:
-#             return 'left'
-#     return binary_search(0, len(nums)-1, condition)
-
-# def last_position(nums, target):
-#     def condition(mid):
-#         if nums[mid] == target:
-#             if mid < len(nums)-1 and nums[mid+1] == target:
-#                 return 'right'
-#             return 'found'
-#         elif nums[mid] < target:
-#             return 'right'
-#         else:
-#             return 'left'
-#     return binary_search(0, len(nums)-1, condition)
-
-# def searchRange(nums, target):
-#     return [first_position(nums, target), last_position(nums, target)]
-
-# nums = [5,7,7,8,8,10]
-# target = 8
-
-# print(searchRange(nums, target))
+            if mid_number == target:
+                if mid < len(nums) - 1 and nums[mid + 1] == target:
+                    return 'right'
+                else:
+                    return 'found'
+            elif mid_number > target:
+                return 'left'
+            elif mid_number < target:
+                return 'right'
+        return self.binary_search(0, len(nums) - 1, condition)
+    
+    # return range
+    def searchRange(self, nums, target):
+        return [self.first_position(nums, target), self.last_position(nums, target)]
